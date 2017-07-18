6e36d783cae08b2e4c2f57756594fd55b7eb3b03
yonatan kreiner <yonatanKreiner@gmail.com>
2017-07-18 20:49:26 +0300

    Added mock api

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/', (req, res) =>
 	res.sendFile(path.join(__dirname, '../src/index.html'))
 )
 
+app.get('/users', (req, res) => {
+	res.json([
+		{"id": 1, "firstName": "Bob", "lastName": "asdf", "email": "asdf"},
+		{"id": 2, "firstName": "Bob", "lastName": "asdf", "email": "asdf"},
+		{"id": 3, "firstName": "Bob", "lastName": "asdf", "email": "asdf"}
+	])
+})
+
 app.listen(port, err => {
 	if (err) {
 		console.log(err)
