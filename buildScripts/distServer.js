8d628b337536a6a73eb4f516234324c08b2bb482
yonatan kreiner <yonatanKreiner@gmail.com>
2017-08-21 23:52:28 +0300

    Add helmet to distServer

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -2,12 +2,14 @@ import express from 'express'
 import path from 'path'
 import open from 'open'
 import compression from 'compression'
+import helmet from 'helmet'
 
 /* eslint-disable no-console */
 
 const port = 3000
 const app = express()
 
+app.use(helmet())
 app.use(compression())
 app.use(express.static('dist'))
 
